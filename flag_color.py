0bd312d00bfed4fd3d88b105a8cc72130196308e
lars <symbolic1337@hotmail.com>
2015-02-22 14:58:37 -0800

    Added superclass FlagColor with return random color function

---
+++ flag_color.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#Lars Backer
+
+import sys
+from random import randrange
+from PyQt4 import QtGui,QtCore
+
+class FlagColor(QtGui,QColor):
+    def __init_(self):
+        super(FlagColor,self).__init__()
+	
+	def getColor():
+		return (randrange(self.setRed(0,255)),randrange(self.setGreen(0,255)),randrange(self.setBlue(0,255)))
+	
+
+def main():
+    app = QApplication(sys.argv)
+    flagcolor = FlagColor()
+    flagcolor.show()
+    sys.exit(app.exec_())
+
+
+if __name
+__ == '__main__':
+    main()
