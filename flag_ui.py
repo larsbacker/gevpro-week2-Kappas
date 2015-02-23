41a437dcddd1dbf98e8821ff13ca508287c337fa
Richards Zheng <r-zh3ng@hotmail.com>
2015-02-23 02:32:39 +0100

    modified flag_ui.py

--- flag_ui.py
+++ flag_ui.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-#Cock
+# Lars Bäcker Richards Zheng
 import sys
 from PyQt4 import QtGui, QtCore
 from country import Country
 from flag_color import FlagColor
 from random import randrange
 
-
+# Function to read the file in and return a list of country names
 def readnames(stdin):
     countries = []
     countries1 = []
@@ -20,51 +20,55 @@ def readnames(stdin):
 
 
 class Example(QtGui.QWidget):
-    
+    # Create class
     def __init__(self):
         super(Example, self).__init__()
         
         self.initUI()
         
-    def initUI(self):
-		          
-		# Draw QLabel and move it      
+    def initUI(self):   
         
-        # Draw Qcomboboxes 1 and 2 and move them
+        # Draw Qcomboboxe 1 and move it
         self.combo1 = QtGui.QComboBox(self)
         self.combo1.move(50, 50)
-        self.col = QtGui.QColor(150, 175, 90)
+        
+        # Draw 3 QFrames and move them
+        self.col = QtGui.QColor(0, 0, 0)
         self.square = QtGui.QFrame(self)
         self.square.setGeometry(50, 90, 240, 30)
         self.square.setStyleSheet("QWidget { background-color: %s }" % self.col.name())
-        self.col1 = QtGui.QColor(125, 133, 45)
+        self.col1 = QtGui.QColor(0, 0, 0)
         self.square1 = QtGui.QFrame(self)
-        self.square1.setGeometry(75, 90, 240, 30)
+        self.square1.setGeometry(50, 115, 240, 30)
         self.square1.setStyleSheet("QWidget { background-color: %s }" % self.col1.name())
-        self.col2 = QtGui.QColor(78, 35, 49)
+        self.col2 = QtGui.QColor(0, 0, 0)
         self.square2 = QtGui.QFrame(self)
-        self.square2.setGeometry(100, 90, 240, 30)
+        self.square2.setGeometry(50, 140, 240, 30)
         self.square2.setStyleSheet("QWidget { background-color: %s }" % self.col2.name())        
-        # Use the function to get the currencies and fill the
-        # Comboboxes with currencies (Euro/Dollars/Peso etc)
-        # Also sorts the lists
-        hallo = readnames(self)
-        self.combo1.addItems(sorted(list(hallo)))
-        self.combo1.currentIndexChanged.connect(self.on1Activated)
+		
+		# Used readnames function to return a list into countrynames variable
+		# Added the list items into the QCombobox
+		# Changing QCombobox index activates flagChanger function
+        countrynames = readnames(self)
+        self.combo1.addItems(sorted(list(countrynames)))
+        self.combo1.currentIndexChanged.connect(self.flagChanger)
 
         # Set window size and draw
-        self.setGeometry(700, 700, 700, 200)
-        self.setWindowTitle('Currency converter')
+        self.setGeometry(350, 350, 350, 200)
+        self.setWindowTitle('Random Flags')
         self.show()
         
-
-        
-        
-    def on1Activated(self):
-        self.kappa = QtGui.QColor(150, 175, 90)
-        a, b, c = FlagColor.getColor(self.kappa)
-        self.col = QtGui.QColor(a, b, c)       
+        # Change the color of the three QFrames
+    def flagChanger(self):
+        a, b, c = randrange(0,255), randrange(0,255), randrange(0,255)
+        a1, b1, c1 = randrange(0,255), randrange(0,255), randrange(0,255)
+        a2, b2, c2 = randrange(0,255), randrange(0,255), randrange(0,255)
+        self.col = QtGui.QColor(a, b, c)
+        self.col1 = QtGui.QColor(a1, b1, c1)
+        self.col2 = QtGui.QColor(a2, b2, c2)
         self.square.setStyleSheet("QWidget { background-color: %s }" % self.col.name())
+        self.square1.setStyleSheet("QWidget { background-color: %s }" % self.col1.name())
+        self.square2.setStyleSheet("QWidget { background-color: %s }" % self.col2.name())
         
 def main():
     app = QtGui.QApplication(sys.argv)
